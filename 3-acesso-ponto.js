6f0dc6facaa3f26ca5f692798f21ae8cd7f9abbf
Emillyvicsilva <140508800+Emillyvicsilva@users.noreply.github.com>
2024-09-17 10:49:30 -0300

    Add files via upload

---
+++ 3-acesso-ponto.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Andre",
+    idadede: 32,
+    cpf: "1122233345",
+    email: "andre@dominio.com",
+};
+console.log(`O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos`)
+console.log(`Os 3 primeiros digitos do CPF são ${cliente.cpf.substring}`)
